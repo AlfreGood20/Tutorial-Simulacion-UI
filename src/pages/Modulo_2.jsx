437f5498b2d80b771d4cd3aa94c4919aa459d2f1
AlfreGood20 <josealfredolopezdelacruz2@gmail.com>
2026-03-14 15:49:13 -0600

    new: cambios y nuevas cosas

--- src/pages/Modulo_2.jsx
+++ src/pages/Modulo_2.jsx
@@ -3,12 +3,17 @@ import MainContent from '../components/MainContent'
 import Sidebar from '../components/Sidebar'
 import Content from '../components/Content'
 import './layout.css'
+import CuadradoMedios from './simuladores/CuadradoMedios';
+import ProductosMedios from './simuladores/ProductosMedios';
+import MultiplicadorConstante from './simuladores/MultiplicadorConstante'
 
 export default function Modulo_2() {
 
-  const [content, setContent] = useState("");
+    const [content, setContent] = useState("");
     const [leccion, setLeccion] = useState("introduccion");
-  
+    const [mostrarSide, setSide] = useState("");
+    const [ejercicio, setEjercicio] = useState("");
+
     useEffect(() => {
       fetch(`/docs/modulo_2/${leccion}.md`) 
         .then((res) => res.text())
@@ -22,16 +27,34 @@ export default function Modulo_2() {
     <MainContent>
       <Sidebar>
 
-        <button className='btn-side'>Introducción</button>
-        <button className='btn-side'>Algoritmo de cuadrados medios</button>
-        <button className='btn-side'>Algorimo de productos medios</button>
-        <button className='btn-side'>Algorimo de multiplicador constante</button> 
+        <div className='mt-10'>
+          <button className='btn-side' onClick={() => {setLeccion("introduccion"); setSide(""); setEjercicio("")}}>Introducción</button>
+        </div>
+
+        <h1 className='ml-5 underline'>Clasificación</h1>
+        <button className='btn-side ml-8' onClick={() => setSide("no congruencial")}>No Congruencial</button>
+        {mostrarSide == "no congruencial" && mostrarSide != "" &&
+          <>
+            <button className='btn-side ml-15' onClick={() => {setEjercicio(""); setLeccion("algoritmo_cuadrados_medios")}}>Cuadrados medios</button>
+            <button className='btn-side ml-25' onClick={() => {setLeccion(""); setEjercicio("cuadrados medios")}}>Simulador</button>
+
+            <button className='btn-side ml-15' onClick={() => {setEjercicio(""); setLeccion("algoritmo_productos_medios")}}>Productos medios</button>
+            <button className='btn-side ml-25' onClick={() => {setLeccion(""); setEjercicio("productos medios")}}>Simulador</button>
+
+            <button className='btn-side ml-15' onClick={()=> {setEjercicio(""); setLeccion("algoritmo_multiplicador_constante")}}>Multiplicador constante</button>
+            <button className='btn-side ml-25' onClick={() => {setLeccion(""); setEjercicio("multiplicador constante")}}>Simulador</button>
+          </>
+        }
+        <button className='btn-side ml-8' onClick={() => setSide("congruencial")}>Congruencial</button>
 
       </Sidebar>
 
-      <Content>
-        {content}
-      </Content>
+      {leccion != "" && <Content>{content}</Content>}
+      {ejercicio == "cuadrados medios" && <CuadradoMedios/>}
+      {ejercicio == "productos medios" && <ProductosMedios />}
+      {ejercicio == "multiplicador constante" && <MultiplicadorConstante />}
+      
+
     </MainContent>
   )
 }
